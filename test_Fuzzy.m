344cfbb260816503bd0562839bd25c7fb0bd9025
Lenovo_Linux <mopolimi@gmail.com>
2024-11-20 17:39:39 +0100

    SO and Fuzzy

--- test_Fuzzy.m
+++ test_Fuzzy.m
@@ -1,7 +1,9 @@
+
 clear;clc
 MeanMat = [];
 VariMat = [];
 Var0 = 1;
+W = [];
 RulesNum = 0;
 MF = @(X,l,i,M,S) gaussmf(X,[S, M]);
 function out = Antc(X,l,M,S,MF)
@@ -18,23 +20,28 @@ for k = 1:50
     disp('*********** No Existed Rule: Add the First Rule ************')
     MeanMat(:,1) = X;
     VariMat(:,1) = ones(length(X),1)*Var0;
+    W(1,1) = rand;
     RulesNum = 1;
-    Antcs(1) = Antc(X,1,MeanMat,VariMat,MF);
+    Antcs(1,1) = Antc(X,1,MeanMat,VariMat,MF);
   else
-    for j = 1:size(MeanMat,2)
-      Antcs(j) = Antc(X,j,MeanMat,VariMat,MF);
+    for l = 1:RulesNum
+      Antcs(l) = Antc(X,l,MeanMat,VariMat,MF);
     end
     if max(Antcs)<0.5
       disp('*********** No Enough Covering: Add a New Rule ************')
-      MeanMat(:,j+1) = X;
-      VariMat(:,j+1) = ones(length(X),1)*Var0;
       RulesNum = RulesNum+1;
-      Antcs(j+1) = Antc(X,j+1,MeanMat,VariMat,MF);    
+      MeanMat(:,RulesNum) = X;
+      VariMat(:,RulesNum) = ones(length(X),1)*Var0;
+      W(RulesNum,1) = rand;
+      Antcs(RulesNum,1) = Antc(X,RulesNum,MeanMat,VariMat,MF);    
     end
   end
 
   disp(['RulesNum : ',num2str(RulesNum)])
   disp(['Max Covering : ', num2str(max(Antcs))])
+
+  Fuzzy_Local_Direction_ref = W'*Antcs;
+  disp(['Fuzzy_Local_Direction_ref = ', num2str(Fuzzy_Local_Direction_ref)])
 
 end
 
