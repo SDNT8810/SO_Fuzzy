39a89e61220580484d350d46310d65f32f595a49
Davoud Nikkhouy <davood_space@yahoo.com>
2024-12-10 23:00:13 +0100

    remove everything !

--- Lidar2Fuzzy.m
+++
@@ -1,9 +0,0 @@
-function o = Lidar2Fuzzy(Points360, Lidar_Augmented, Membership_Lidar, MF_Lidar_, Dist_MF_L2F, Num_MF_L2F, MF_L2F, Max_Lidar)
-  Y = Lidar_Augmented(Points360);
-  for j = 1:Num_MF_L2F
-    y = Y((j-1)*Dist_MF_L2F+1: (j+1)*Dist_MF_L2F+1);
-    Membership_Lidar(:,j) = MF_L2F.*y;
-    MF_Lidar_(j,1) = sum(Membership_Lidar(:,j));
-  end
-  o = MF_Lidar_/Max_Lidar;
-end
